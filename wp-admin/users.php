b2ee3c181f9f93a1232367bb727f296bb5d83378
Munshi91 <mimunshi91@gmail.com>
2024-01-27 12:18:18 +0600

    user Data View in Data Base

--- wp-admin/users.php
+++ wp-admin/users.php
@@ -12,11 +12,85 @@ include ("./includes/header.php")
   
             //View All data
             if ($do == 'Manage'){
-              echo 'All Data Show';
+              ?>
+    <div class="row">
+      <div class="col-lg-12 col-md-12">
+        <div class="card">
+          <div class="card-header card-header-warning">
+            <h4 class="card-title">View All User</h4>
+          </div>
+          <div class="card-body table-responsive">
+            <table class="table table-hover">
+              <thead class="text-warning">
+                <th>Serial</th>
+                <th>Photo</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+                <th>Bio Data</th>
+                <th>User Role</th>
+                <th>Action</th>
+              </thead>
+              <tbody>
+                <?php
+              $sql = "SELECT * FROM wp_user";
+              $result = mysqli_query($db, $sql);
+              $count = 0 ;
+                while ($row = mysqli_fetch_assoc($result)) {
+                  $u_id = $row["u_id"];                  
+                  $u_name = $row["u_name"];
+                  $u_email= $row["u_email"];
+                  $u_phone = $row["u_phone"];
+                  $u_address= $row["u_address"];
+                  $biodata = $row["biodata"];
+                  $photo = $row["photo"];
+                  $u_role= $row["u_role"];
+                  $count ++;
+                  ?>
+                <tr>
+                  <td><?php echo $count?></td>
+                  <td><?php echo  $photo?></td>
+                  <td><?php echo $u_name?></td>
+                  <td><?php echo $u_email?></td>
+                  <td><?php echo$u_phone?></td>
+                  <td><?php echo  $u_address?></td>
+                  <td><?php echo  $biodata?></td>
+                  <td><?php 
+                  
+                  if ($u_role == "1"){
+                    echo '<span class="">Admin</span>';
+                  }
+                  if ($u_role == "0") {
+                    echo '<span class="">Subscriber</span>';
+                  }
+                  
+                  ?></td>
+                  <td class="td-actions text-center">
+                    <a type="button" href="users.php?editId= <?php echo $u_id ?>" rel="tooltip" title="Edit"
+                      class="btn btn-primary btn-link btn-sm">
+                      <i class="material-icons">edit</i>
+                    </a>
+                    <a type="button" href="#" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
+                      <i class="material-icons">close</i>
+                    </a>
+                  </td>
+                </tr>
+                <?php
+                }
+              ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php 
             }
             //Add New  data
             elseif ($do == 'add'){
-        echo "Add";
+
 
             } 
             // Edit  data
